8b76fdf3c7875fa18a1e2eedc08d0ec7be3b16a8
murderew <47886572+murderew@users.noreply.github.com>
2019-05-30 23:13:21 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+
+void Increase(int s, int mas[]);
+void reverse(int s, int mas[]);
+int sum(int s, int mas[]);
+void(*Function(int s, int mas[]))(int, int[]);
+
+#endif 
